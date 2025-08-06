75c60b662a67bf977b14b6e41c0f65855d81bf19
swyswyswyswy <wenyu.song@innoflexbv.com>
2025-08-06 12:41:10 +0100

    update parameters.py

--- parameters.py
+++ parameters.py
@@ -3,22 +3,22 @@
 
 params = {
     'pin':{ # BCM
-        'pin_enable':12,
-        'pin_step':21,
-        'pin_direction':20
+        'pin_enable'   : 12,
+        'pin_step'     : 21,
+        'pin_direction': 20
     },
     'device':{
-        'stepsize':0.9, #[degree]
-        'lead':5 # [mm]
+        'stepsize'  : 0.9, #[degree]
+        'lead'      : 5 # [mm]
     },
     'motor':{
-        'down_speed':10, # [mm/s]
-        'up_speed':1, #[mm/s]
-        'low_stay':1, # [s] time stay at sinking position
-        'high_stay':1, #[s]
-        'repeat':3
+        'down_speed': 10, # [mm/s]
+        'up_speed'  : 0.10, #[mm/s]
+        'low_stay'  : 2, # [s] time stay at sinking position
+        'high_stay' : 0.5, #[s]
+        'repeat'    : 3
     },
     'distance':{
-        'distance': 40 # [mm] moving distance of sample
+        'distance'  : 10 # [mm] moving distance of sample
     }
 }
