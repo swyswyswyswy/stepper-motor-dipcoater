03971947e447aee7260d918fbba1533f951e7b0a
swyswyswyswy <wenyu.song@innoflexbv.com>
2025-08-06 11:15:25 +0200

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,24 @@
+# parameters for motor movers
+
+
+params = {
+    'pin':{ # BCM
+        'pin_enable':12,
+        'pin_step':21,
+        'pin_direction':20
+    }
+    'device':{
+        'stepsize':0.9, #[degree]
+        'lead':5 # [mm]
+    }
+    'motor':{
+        'down_speed':10, # [mm/s]
+        'up_speed':1, #[mm/s]
+        'low_stay':1, # [s] time stay at sinking position
+        'high_stay':1, #[s]
+        'repeat':3
+    }
+    'distance':{
+        'distance': 50 # [mm] moving distance of sample
+    }
+}
